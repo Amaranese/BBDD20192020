460e5e2a5524d52e4d954f346c23c3ad83c46b63
Alejandro Marañes <alejandromaranes@gmail.com>
2020-04-12 16:27:44 +0200

    .

---
+++ usuariosycontraseñas/app/Category.php
@@ -0,0 +1,16 @@
+<?php 
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class Category extends Model
+{
+	protected $table = 'categories';
+	protected $fillable = ['name'];
+	public function password()
+    {
+    	return $this->hasMany('\App\Password');
+    }
+    public function user()
+    {
+    	return $this->belongsTo('\App\User');
+    }
+}
---
+++ usuariosycontraseñas/app/Role.php
@@ -0,0 +1,12 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class Role extends Model
+{
+	protected $table = 'roles';
+	protected $fillable = 'name';
+	public function users()
+    {
+    	return $this->hasMany('App\User');
+    }
+}
